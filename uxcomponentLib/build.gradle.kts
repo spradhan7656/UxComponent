885fb79488f807729f3eb068fdb9de296011bcbc
spradhan7656 <spradhan7656@gmail.com>
2026-09-03 18:05:27 +0530

    Configure Maven publishing for JitPack

--- uxcomponentLib/build.gradle.kts
+++ uxcomponentLib/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     alias(libs.plugins.android.library)
+    `maven-publish`
 }
 
 android {
     namespace = "com.spradhan.uxcomponentLib"
+
     compileSdk {
         version = release(37) {
             minorApiLevel = 1
@@ -14,18 +16,39 @@ android {
         minSdk = 21
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.spradhan7656"
+            artifactId = "uxcomponentLib"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
