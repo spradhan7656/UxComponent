bc3eb217c09a32e436c805cd435e62d480d334ae
spradhan7656 <spradhan7656@gmail.com>
2026-09-09 14:16:06 +0530

    added animated butt

--- uxcomponentLib/build.gradle.kts
+++ uxcomponentLib/build.gradle.kts
@@ -48,7 +48,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.spradhan7656"
             artifactId = "uxcomponentLib"
-            version = "1.0.4"
+            version = "1.0.5"
 
             afterEvaluate {
                 from(components["release"])
