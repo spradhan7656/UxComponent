a7fe11215391c5ca062cf0c1c943562cca338fe4
spradhan7656 <spradhan7656@gmail.com>
2026-09-15 10:06:50 +0530

    changed tag

--- uxcomponentLib/build.gradle.kts
+++ uxcomponentLib/build.gradle.kts
@@ -48,7 +48,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.spradhan7656"
             artifactId = "uxcomponentLib"
-            version = "1.0.7"
+            version = "1.0.8"
 
             afterEvaluate {
                 from(components["release"])
